3e58a3531b71ef9f85380cbc126a2c03a22eb009
Jair Neri <yayodcv@gmail.com>
2021-01-10 23:36:04 -0600

    Setup Home mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const Layout = () => <div className="layout">Home</div>;
+const Layout = () => (
+  <div className="layout">
+    <div>Logos</div>
+    <div className="Hero">
+      <figure>
+        <img src="" alt="catwiki" />
+      </figure>
+      <small>Get to know more about your cat breed</small>
+      <div>
+        <p>Search</p>
+        <small>♟</small>
+      </div>
+      <button>search </button>
+    </div>
+  </div>
+);
 
 export default Layout;
