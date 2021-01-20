d40fd9f93b34b8381dbac197913540cbf7e54c9c
Jair Neri <yayodcv@gmail.com>
2021-01-20 12:42:53 -0600

    Hero styles setup

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,41 @@ const Hero = () => (
         <p>Get to know more about your cat breed</p>
         <button>search </button>
       </div>
-      <div>
-        <figure>
-          <img src="" alt="Gatopng" />
-        </figure>
+      <figure>
+        <img src="" alt="Gatopng" />
+      </figure>
+    </section>
+    <section className="galery">
+      <h3> Most Searched Breeds</h3>
+      <hr />
+      <h2>66+ Breeds For you to discover</h2>
+      <div className="galery_img">
+        <div>
+          <figure>
+            <img src="" alt="g1" />
+          </figure>
+          <p>Bengal</p>
+        </div>
+        <div>
+          <figure>
+            <img src="" alt="g2" />
+          </figure>
+          <p>Savannah</p>
+        </div>
+        <div>
+          <figure>
+            <img src="" alt="g3" />
+          </figure>
+          <p>Norwegian Forest Cat</p>
+        </div>
+        <div>
+          <figure>
+            <img src="" alt="g4" />
+          </figure>
+          <p>Selkirk Rex</p>
+        </div>
       </div>
     </section>
-    <section className="galery">galery</section>
   </div>
 );
 export default Hero;
