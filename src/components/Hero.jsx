807d121c1ebb6bfd719033ef174b60d0e7350b89
Jair Neri <yayodcv@gmail.com>
2021-01-24 01:03:30 -0600

    Setup Tablet breakpoint

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
+
 const Hero = () => (
   <div className="hero">
     <section className="baner">
@@ -7,7 +9,10 @@ const Hero = () => (
           <img src="" alt="catwiki" />
         </figure>
         <p>Get to know more about your cat breed</p>
-        <button>search </button>
+        <button>
+          search
+          <MdSearch />
+        </button>
       </div>
       <figure>
         <img src="" alt="Gatopng" />
@@ -45,18 +50,22 @@ const Hero = () => (
       </div>
     </section>
     <section className="post">
-      <h2>Why should you have a cat?</h2>
-      <p>
-        Having a cat around you can actually trigger the release of calming
-        chemicals in your body which lower your stress and anxiety leves
-      </p>
-      <p className="link"> READ MORE </p>
-      <div className="cats">
-        <figure>
-          <img src="" alt="cat1" />
-          <img src="" alt="cat2" />
-          <img src="" alt="cat3" />
-        </figure>
+      <div className="row1">
+        <h2>Why should you have a cat?</h2>
+        <p>
+          Having a cat around you can actually trigger the release of calming
+          chemicals in your body which lower your stress and anxiety leves
+        </p>
+        <button className="link">READ MORE </button>
+      </div>
+      <div className="row">
+        <div className="cats">
+          <figure>
+            <img src="" alt="cat1" />
+            <img src="" alt="cat2" />
+            <img src="" alt="cat3" />
+          </figure>
+        </div>
       </div>
     </section>
   </div>
